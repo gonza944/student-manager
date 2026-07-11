912e1caaf5f03ff9edb1246ffae29ddb6214ad9b
Gonzalo Ariza <gonza94.4@gmail.com>
2026-07-11 13:11:04 -0300

    Add OpenNext and Wrangler support with updated configurations and dependencies

---
+++ db/index.ts
@@ -0,0 +1,11 @@
+import "server-only";
+
+import { getCloudflareContext } from "@opennextjs/cloudflare";
+import { drizzle } from "drizzle-orm/d1";
+import * as schema from "./schema";
+
+export async function getDb() {
+  const { env } = await getCloudflareContext({ async: true });
+
+  return drizzle(env.DB, { schema });
+}
